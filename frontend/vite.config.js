75588264460f618dd77c01cc7fb29ca5113660b2
Imakp <theankitkumarpandit@gmail.com>
2025-10-23 18:54:39 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  
+
   return {
     plugins: [
       react({
@@ -11,6 +12,7 @@ export default defineConfig(({ mode }) => {
           plugins: [['babel-plugin-react-compiler']],
         },
       }),
+      tailwindcss(),
     ],
     server: {
       proxy: {
